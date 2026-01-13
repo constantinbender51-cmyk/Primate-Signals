4a38076c81d972a1e9c83a2047a582ed1bd84d6c
Constantin Bender <constantinbender51@gmail.com>
2026-01-13 17:25:57 +0100

    Update main.jsx

--- API/client/src/main.jsx
+++ API/client/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-[span_0](start_span)import './index.css' //[span_0](end_span) Added this import
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
