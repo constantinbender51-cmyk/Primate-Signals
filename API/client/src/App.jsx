c21b51101ea1ddc420429acbc696d5a918922c56
Constantin Bender <constantinbender51@gmail.com>
2026-01-13 17:22:36 +0100

    Update App.jsx

--- API/client/src/App.jsx
+++ API/client/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './Layout'; // Import the new layout
 import Login from './Login';
 import Register from './Register';
 import Dashboard from './Dashboard';
@@ -7,12 +8,14 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/" element={<Dashboard />} />
+        <Route element={<Layout />}>
+           <Route path="/login" element={<Login />} />
+           <Route path="/register" element={<Register />} />
+           <Route path="/" element={<Dashboard />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
-}
+[span_1](start_span)} //[span_1](end_span)
 
 export default App;
