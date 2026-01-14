1464059250f22de328da85f99a59b877d2dbdc3b
Constantin Bender <constantinbender51@gmail.com>
2026-01-14 21:10:32 +0100

    AI Update to API/client/src/App.jsx

--- API/client/src/App.jsx
+++ API/client/src/App.jsx
@@ -4,6 +4,7 @@ import Login from './Login';
 import Register from './Register';
 import Dashboard from './Dashboard';
 import LegalTextPage from './LegalTextPage';
+import APIDocs from './APIDocs';
 
 function App() {
   return (
@@ -15,6 +16,8 @@ function App() {
            <Route path="/" element={<Dashboard />} />
            {/* Legal Pages */}
            <Route path="/legal/:type" element={<LegalTextPage />} />
+           {/* API Documentation */}
+           <Route path="/api-docs" element={<APIDocs />} />
         </Route>
       </Routes>
     </BrowserRouter>
