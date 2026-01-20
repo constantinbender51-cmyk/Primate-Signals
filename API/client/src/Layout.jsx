6b9190a207ed5dd1bc0f7835616e2b083927b7cf
Constantin Bender <constantinbender51@gmail.com>
2026-01-20 23:33:15 +0100

    Update Layout.jsx

--- API/client/src/Layout.jsx
+++ API/client/src/Layout.jsx
@@ -58,7 +58,7 @@ export default function Layout() {
         <span style={{ fontWeight: '300' }}>Signals</span><img 
           src="https://raw.githubusercontent.com/constantinbender51-cmyk/Models/main/icons/vg2.png" 
           alt="Stone age guy icon" 
-          style={{ height: '48px', margin: '0 -8px' }} 
+          style={{ height: '48px', margin: '0 -7px' }} 
         />
     </Link>
               <nav style={{ display: 'flex', gap: '1.5rem', fontSize: '14px', fontWeight: '500' }}>
