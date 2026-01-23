29100f81b0a34d27e4afcf74af69d64ea8157715
Constantin Bender <constantinbender51@gmail.com>
2026-01-23 23:01:33 +0100

    Update App.jsx

--- API/client/src/App.jsx
+++ API/client/src/App.jsx
@@ -4,23 +4,30 @@ import Login from './Login';
 import Register from './Register';
 import Dashboard from './Dashboard';
 import Profile from './Profile';
-import AssetDetails from './AssetDetails'; // Import new Component
+import AssetDetails from './AssetDetails';
 import LegalTextPage from './LegalTextPage';
 import APIDocs from './APIDocs';
+import LandingPage from './LandingPage'; // Import the new page
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route element={<Layout />}>
+           {/* Public / Landing */}
+           <Route path="/" element={<LandingPage />} />
+           
+           {/* App Routes */}
+           <Route path="/dashboard" element={<Dashboard />} />
+           <Route path="/asset/:symbol" element={<AssetDetails />} />
+           
+           {/* Auth & User */}
            <Route path="/login" element={<Login />} />
            <Route path="/register" element={<Register />} />
-           <Route path="/" element={<Dashboard />} />
-           <Route path="/asset/:symbol" element={<AssetDetails />} /> {/* New Route */}
            <Route path="/profile" element={<Profile />} />
-           {/* Legal Pages */}
+           
+           {/* Resources */}
            <Route path="/legal/:type" element={<LegalTextPage />} />
-           {/* API Documentation */}
            <Route path="/api-docs" element={<APIDocs />} />
         </Route>
       </Routes>
