c3ce65a8ce81e6812fd79fe4c23003dde8d4c6bd
Constantin Bender <constantinbender51@gmail.com>
2026-01-24 16:02:53 +0100

    Update Layout.jsx

--- API/client/src/Layout.jsx
+++ API/client/src/Layout.jsx
@@ -55,7 +55,7 @@ export default function Layout() {
         <img 
           src="https://i.postimg.cc/yxFD7hbT/IMG-20260124-160140.png" 
           alt="voodooghost" 
-          style={{ height: '24px', margin: '0 -12px' }} 
+          style={{ height: '32px', margin: '0 0px' }} 
         />Primate 
         {/* Updated Image Source */}
         
